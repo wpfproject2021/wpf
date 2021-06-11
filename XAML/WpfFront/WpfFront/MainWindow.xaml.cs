1b861d378010d983738d1188a642aa7ae3b786e0
jasminesr2001 <jasminesr2001@gmail.com>
2021-06-11 18:09:34 +0430

    login

--- XAML/WpfFront/WpfFront/MainWindow.xaml.cs
+++ XAML/WpfFront/WpfFront/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -33,6 +34,40 @@ namespace WpfFront
         private void btn_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        //a method to check the validity of given information
+        private bool valid(string user, string pass)
+        {
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(user);
+
+            if (pass.Length < 8)
+            {
+                MessageBox.Show("Your password is less than 8 characters.");
+                return false;
+            }
+            if (!match.Success)
+            {
+                MessageBox.Show("Email format is incorrect");
+                return true;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private void register_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            //first checks if the password and username are in valid format
+            string userName = username.Text;
+            string pass = password.Password;
+            valid(userName, pass);
         }
     }
 }
